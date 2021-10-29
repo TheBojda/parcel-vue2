af049e0cdf6c4c582f3231f6a7c3f9010aadf0a0
Laszlo Fazekas <thebojda@gmail.com>
2021-10-29 10:37:28 +0200

    a more complex example, using bootstrap vue

--- example/app.js
+++ example/app.js
@@ -1,4 +1,10 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue';
 
+Vue.use(BootstrapVue);
 new Vue({ render: createElement => createElement(App) }).$mount('#app');
